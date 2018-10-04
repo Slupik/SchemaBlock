00b724318fdfd1582b9c0f94f42bd72b24e8493d
Slupik <Slupik98@gmail.com>
2018-10-04 18:59:45 +0200

    feat: Add base for add method

---
+++ Parser/src/io/github/slupik/schemablock/parser/math/pattern/specific/MathPatternAdd.java
@@ -0,0 +1,84 @@
+package io.github.slupik.schemablock.parser.math.pattern.specific;
+
+import io.github.slupik.schemablock.parser.math.pattern.InvalidArgumentsException;
+import io.github.slupik.schemablock.parser.math.pattern.MathPattern;
+import io.github.slupik.schemablock.parser.math.pattern.UnsupportedValueException;
+import io.github.slupik.schemablock.parser.math.value.Value;
+import io.github.slupik.schemablock.parser.math.value.ValueType;
+
+import static io.github.slupik.schemablock.parser.math.value.ValueType.DOUBLE;
+import static io.github.slupik.schemablock.parser.math.value.ValueType.INT;
+import static io.github.slupik.schemablock.parser.math.value.ValueType.SHORT;
+
+/**
+ * All rights reserved & copyright ©
+ */
+public class MathPatternAdd extends MathPattern {
+
+    public MathPatternAdd() {
+        super("add");
+    }
+
+    @Override
+    public Object calculate(Value... args) throws InvalidArgumentsException, UnsupportedValueException {
+        if(isValidArgs(args)) {
+            double result = 0;
+            ValueType biggestType = ValueType.SHORT;
+            for(int i=0;i<args.length;i++) {
+                Object val = args[i].getValue();
+                ValueType type = ValueType.getType(val);
+                switch (type) {
+                    case DOUBLE: {
+                        biggestType = DOUBLE;
+                        result += ((Double) val);
+                        break;
+                    }
+                    case INT: {
+                        if(biggestType==SHORT) {
+                            biggestType = INT;
+                        }
+                        result += ((Integer) val);
+                        break;
+                    }
+                    case SHORT: {
+                        result += ((Short) val);
+                        break;
+                    }
+                    default: {
+                        throw new UnsupportedValueException();
+                    }
+                }
+            }
+            switch (biggestType) {
+                case DOUBLE: {
+                    return ((double) result);
+                }
+                case INT: {
+                    return ((int) result);
+                }
+                case SHORT: {
+                    return ((short) result);
+                }
+            }
+            return result;
+        } else {
+            throw new InvalidArgumentsException();
+        }
+    }
+
+    @Override
+    public boolean isValidArgs(Value... args) {
+        for(int i=0;i<args.length;i++) {
+            Value val = args[i];
+            if(!ValueType.isNumber(val.getType())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int maxArgs() {
+        return -1;
+    }
+}
