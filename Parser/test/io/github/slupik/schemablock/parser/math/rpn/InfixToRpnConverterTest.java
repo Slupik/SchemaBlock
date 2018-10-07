4c9e12312059c82ffbb6dff5a811b5c71301c93f
Slupik <Slupik98@gmail.com>
2018-10-07 23:35:17 +0200

    test: Bugfixed unit tests fot Infix to RPN converter

--- Parser/test/io/github/slupik/schemablock/parser/math/rpn/InfixToRpnConverterTest.java
+++ Parser/test/io/github/slupik/schemablock/parser/math/rpn/InfixToRpnConverterTest.java
@@ -18,9 +18,9 @@ class InfixToRpnConverterTest {
     void convertInfixToRPN() {
         checkCase("[41, 13, +]", new String[]{"41", "+", "13"});
         checkCase("[2, 3, 4, 2, /, +, *]", new String[]{"2", "*", "(", "3", "+", "4", "/", "2", ")"});
-        checkCase("[4, 5, *, 3, 2, -, *]", "4 * 5 * ( 3 - 2 )".split(" "));
-        checkCase("[4, 3, sqrt;1, *, 3, 2, -, *]", "4 * sqrt ( 3 ) * ( 3 - 2 )".split(" "));
-        checkCase("[4, 3.2, sqrt;1, *, 3, 2, -, *]", "4 * sqrt ( 3.2 ) * ( 3 - 2 )".split(" "));
+        checkCase("[4, 5, 3, 2, -, *, *]", "4 * 5 * ( 3 - 2 )".split(" "));
+        checkCase("[4, 3, sqrt;1, 3, 2, -, *, *]", "4 * sqrt ( 3 ) * ( 3 - 2 )".split(" "));
+        checkCase("[4, 3.2, sqrt;1, 3, 2, -, *, *]", "4 * sqrt ( 3.2 ) * ( 3 - 2 )".split(" "));
         checkCase("[4, 3, sqrt;1, *, 3, 3, 2, -, *, +]", "4 * sqrt ( 3 ) + 3 * ( 3 - 2 )".split(" "));
     }
 
