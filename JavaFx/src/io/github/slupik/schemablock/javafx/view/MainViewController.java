c1f71e69e041c7c31804ca0bf0fd5e0883a4b965
Slupik <Slupik98@gmail.com>
2018-11-01 20:10:39 +0100

    feat: Autospawn start block

--- JavaFx/src/io/github/slupik/schemablock/javafx/view/MainViewController.java
+++ JavaFx/src/io/github/slupik/schemablock/javafx/view/MainViewController.java
@@ -1,10 +1,12 @@
 package io.github.slupik.schemablock.javafx.view;
 
 import io.github.slupik.schemablock.javafx.element.UiElementType;
-import io.github.slupik.schemablock.javafx.element.fx.arrow.Arrow;
-import io.github.slupik.schemablock.javafx.element.fx.arrow.Line;
+import io.github.slupik.schemablock.javafx.element.fx.UiElementBase;
 import io.github.slupik.schemablock.javafx.logic.drag.icon.DragGhostIcon;
 import io.github.slupik.schemablock.javafx.logic.drag.icon.GhostDragController;
+import io.github.slupik.schemablock.javafx.logic.drag.node.DraggableNode;
+import io.github.slupik.schemablock.javafx.logic.drag.node.NodeDragController;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -61,18 +63,24 @@ public class MainViewController implements Initializable {
     private void setupDragging() {
         ghost = new GhostDragController(mainContainer, sheet, new GhostDragElementFactoryImpl());
         addIconsToMenu();
-        testAddArrow();
+        spawnStartElement();
     }
 
-    private void testAddArrow() {
-        Arrow line = new Arrow();
-        sheet.getChildren().addAll(line);
-        line.setStart(50, 100);
-        line.setEnd(100, 100);
+    private void spawnStartElement() {
+        Platform.runLater(()->{
+            UiElementBase start = UiElementFactory.createByType(UiElementType.START);
+            sheet.getChildren().add(start);
+            if(sheet.getWidth()<150){
+                start.setLayoutX(0);
+            } else {
+                start.setLayoutX(100);
+            }
+            start.setLayoutY(0);
+            new NodeDragController(new DraggableNode(start, false));
+        });
     }
 
     private void addIconsToMenu() {
-        addDragDetection(new DragGhostIconUiElement(UiElementType.START));
         addDragDetection(new DragGhostIconUiElement(UiElementType.STOP));
         addDragDetection(new DragGhostIconUiElement(UiElementType.CALCULATION));
         addDragDetection(new DragGhostIconUiElement(UiElementType.IF));
