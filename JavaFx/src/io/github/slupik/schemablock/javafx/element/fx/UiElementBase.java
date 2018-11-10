d1ab3048cc44e27a1c6dd3de922cb99e10ce0c2b
Slupik <Slupik98@gmail.com>
2018-11-10 18:04:19 +0100

    refactor: Remove commented out code

--- JavaFx/src/io/github/slupik/schemablock/javafx/element/fx/UiElementBase.java
+++ JavaFx/src/io/github/slupik/schemablock/javafx/element/fx/UiElementBase.java
@@ -1,26 +1,19 @@
 package io.github.slupik.schemablock.javafx.element.fx;
 
 import com.google.gson.Gson;
-import com.sun.xml.internal.bind.v2.TODO;
 import io.github.slupik.schemablock.javafx.element.ElementSizeBinder;
 import io.github.slupik.schemablock.javafx.element.UiElement;
 import io.github.slupik.schemablock.javafx.element.UiElementType;
 import io.github.slupik.schemablock.javafx.element.WrongTypeOfElement;
 import io.github.slupik.schemablock.javafx.element.background.CustomShapeBase;
-import io.github.slupik.schemablock.javafx.element.fx.connection.BlockConnector;
 import io.github.slupik.schemablock.model.ui.abstraction.container.ElementContainer;
-import io.github.slupik.schemablock.model.ui.abstraction.element.ConditionalElement;
 import io.github.slupik.schemablock.model.ui.abstraction.element.Element;
-import io.github.slupik.schemablock.model.ui.abstraction.element.OperationElement;
-import io.github.slupik.schemablock.model.ui.abstraction.element.StartElement;
 import io.github.slupik.schemablock.model.ui.implementation.container.ElementInContainerNotFound;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * All rights reserved & copyright ©
@@ -31,10 +24,6 @@ public abstract class UiElementBase extends Pane implements UiElement {
     private Element element;
 
     public UiElementBase(){
-        this(null);
-    }
-
-    public UiElementBase(BlockConnector connector){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(getResourcePath()));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
@@ -48,46 +37,7 @@ public abstract class UiElementBase extends Pane implements UiElement {
         onPreInit();
         init();
         onPostInit();
-        addEventFilter(MouseEvent.MOUSE_ENTERED, event -> {
-//            List<Double> list = getIOPointsList();
-//            for(int i=0;i<list.size();i+=2) {
-//                TODO zdecydować czy ten element będzie się łączył z innym czy inny do tego
-//                connector.spawnPointAt(list.get(i), list.get(i+1), this, (outputElement, isConnectionForTrue) -> {
-//                    switch (outputElement.getType()) {
-//                        case START: {
-//                            This cannot be next element of anything
-//                            return;
-//                        }
-//                        case IF: {
-//                            if(isConnectionForTrue) {
-//                                ((ConditionalElement) outputElement.getLogicElement()).setOnTrue(element.getId());
-//                            }
-//                        }
-//                    }
-//                    if(this instanceof ConditionalElement) {
-//                        if(isConnectionForTrue) {
-//                            ((ConditionalElement) this).setOnTrue(outputElement.getId());
-//                        } else {
-//                            ((ConditionalElement) this).setOnTrue(outputElement.getId());
-//                        }
-//                    } else if(this instanceof OperationElement) {
-//                        ((OperationElement) this).setNextElement(outputElement.getId());
-//                    } else if(this instanceof StartElement) {
-//                        ((StartElement) this).setNextElement(outputElement.getId());
-//                    } else if(this instanceof Element) {
-//                        Element obj = (Element) this;
-//                        switch (obj.getType()) {
-//                            case START: {
-//
-//                            }
-//                        }
-//                    }
-//                });
-//            }
-        });
     }
-
-//    protected abstract List<Double> getIOPointsList();
 
     private void initBackground() {
         CustomShapeBase base = createBackgroundElement();
