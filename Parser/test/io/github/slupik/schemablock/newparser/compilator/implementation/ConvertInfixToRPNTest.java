1b5e9e4a337db046b01d8162f80ed0fccdfba535
Slupik <Slupik98@gmail.com>
2019-02-13 02:31:49 +0100

    test: Adjust tests to the new way of saving array brackets

--- Parser/test/io/github/slupik/schemablock/newparser/compilator/implementation/ConvertInfixToRPNTest.java
+++ Parser/test/io/github/slupik/schemablock/newparser/compilator/implementation/ConvertInfixToRPNTest.java
@@ -21,14 +21,14 @@ class ConvertInfixToRPNTest {
         check("  ( 2  + \t 3) *  a  ", "2", "3", "+", "a", "*");
         check("(2+3)%5", "2", "3", "+", "5", "%");
 
-        check("b=(2+3)*5", "b", "2", "3", "+", "5", "*", "=");
-        check("b=(2+3)*5", "b", "2", "3", "+", "5", "*", "=");
+        check("b=(2+3)*5", "b", "=", "2", "3", "+", "5", "*", "=");
+        check("b=(2+3)*5", "b", "=", "2", "3", "+", "5", "*", "=");
 
-        check("double b=(2+3)*5", "double", "b", "2", "3", "+", "5", "*", "=");
+        check("double b=(2+3)*5", "double", "b", "=", "2", "3", "+", "5", "*", "=");
 
-        check("double[][] b=(2+3)*5", "double", "[]", "[]", "b", "2", "3", "+", "5", "*", "=");
-        check("double[][] b=(c=4)*5", "double", "[]", "[]", "b", "c", "4", "=", "5", "*", "=");
-        check("double b=(c=4+7)*5", "double", "b", "c", "4", "7", "+", "=", "5", "*", "=");
+        check("double[][] b=(2+3)*5", "double", "[]", "[]", "b", "=", "2", "3", "+", "5", "*", "=");
+        check("double[][] b=(c=4)*5", "double", "[]", "[]", "b", "=", "c", "=", "4", "=", "5", "*", "=");
+        check("double b=(c=4+7)*5", "double", "b", "=", "c", "=", "4", "7", "+", "=", "5", "*", "=");
 
         check("sqrt(3)", "3", "1", "sqrt");
         check("sqrt(3, 2)", "3", "2", "2", "sqrt");
@@ -36,18 +36,18 @@ class ConvertInfixToRPNTest {
         check("(2+sqrt(3, 2))*5", "2", "3", "2", "2", "sqrt", "+", "5", "*");
         check("(2+sqrt(7+8, 2))*5", "2", "7", "8", "+", "2", "2", "sqrt", "+", "5", "*");
         check("(2+sqrt((7+8)*9, 2))*5", "2", "7", "8", "+", "9", "*", "2", "2", "sqrt", "+", "5", "*");
-        check("double[32] b=6", "double", "32", "[0]", "b", "6", "=");
-        check("double[8+11] b=6", "double", "8", "11", "+", "[0]", "b", "6", "=");
-        check("double[32][47] b=6", "double", "32", "[0]", "47", "[0]", "b", "6", "=");
-        check("double[8+11][98+34] b=6", "double", "8", "11", "+", "[0]", "98", "34", "+", "[0]", "b", "6", "=");
-        check("double[a[5]+3] b=6", "double", "a", "5", "[1]", "3", "+", "[0]", "b", "6", "=");
-        check("double[][] b=new double[5][3]", "double", "[]", "[]", "b", "new", "double", "5", "[0]", "3", "[0]", "=");
-        check("double[] b = {1.3}", "double", "[]", "b", "1", "{", "1.3", "}", "=");
+        check("double[32] b=6", "double", "[0", "32", "0]", "b", "=", "6", "=");
+        check("double[8+11] b=6", "double", "[0", "8", "11", "+", "0]", "b", "=", "6", "=");
+        check("double[32][47] b=6", "double", "[0", "32", "0]", "[0", "47", "0]", "b", "=", "6", "=");
+        check("double[8+11][98+34] b=6", "double", "[0", "8", "11", "+", "0]", "[0", "98", "34", "+", "0]", "b", "=", "6", "=");
+        check("double[a[5]+3] b=6", "double", "[0", "a", "[1", "5", "1]", "3", "+", "0]", "b", "=", "6", "=");
+        check("double[][] b=new double[5][3]", "double", "[]", "[]", "b", "=", "new", "double", "[0", "5", "0]", "[0", "3", "0]", "=");
+        check("double[] b = {1.3}", "double", "[]", "b", "=", "1", "{", "1.3", "}", "=");
         check("{1.3}", "1", "{", "1.3", "}");
-        check("double[] b = {1.3, 23.4, 54.2}", "double", "[]", "b", "3", "{", "1.3", ",", "23.4", ",", "54.2", "}", "=");
-        check("double[] b = {{1.3}, 23.4}", "double", "[]", "b", "2", "{", "1", "{", "1.3", "}", ",", "23.4", "}", "=");
+        check("double[] b = {1.3, 23.4, 54.2}", "double", "[]", "b", "=", "3", "{", "1.3", ",", "23.4", ",", "54.2", "}", "=");
+        check("double[] b = {{1.3}, 23.4}", "double", "[]", "b", "=", "2", "{", "1", "{", "1.3", "}", ",", "23.4", "}", "=");
         check("double b, c", "double", "b", ",", "c");
-        check("double name[] = a[8]+b", "double", "name", "[]", "a", "8", "[0]", "b", "+", "=");
+        check("double name[] = a[8]+b", "double", "name", "[]", "=", "a", "[0", "8", "0]", "b", "+", "=");
     }
 
     @Test
@@ -65,12 +65,7 @@ class ConvertInfixToRPNTest {
 
         Queue<Token> queue = new LinkedList<>(ConvertInfixToRPN.convertInfixToRPN(cleared));
         for(int i=0;queue.size()>0;i++) {
-            if(!queue.peek().isSpecialToken()) {
-                Assertions.assertEquals(excepted[i], queue.poll().getData());
-            } else {
-                i--;
-                queue.poll();
-            }
+            Assertions.assertEquals(excepted[i], queue.poll().getData());
         }
         Assertions.assertEquals(0, queue.size());
     }
