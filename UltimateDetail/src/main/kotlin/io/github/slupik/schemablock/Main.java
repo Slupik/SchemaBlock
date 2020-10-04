4be09c39769f345a975e361ad5878da862858086
Slupik <Slupik98@gmail.com>
2020-10-04 21:35:02 +0200

    feat: Run new ui version

--- UltimateDetail/src/main/kotlin/io/github/slupik/schemablock/Main.java
+++ UltimateDetail/src/main/kotlin/io/github/slupik/schemablock/Main.java
@@ -1,9 +1,17 @@
 package io.github.slupik.schemablock;
 
+import de.tesis.dynaware.grapheditor.demo.GraphEditorDemo;
+
 public class Main {
 
+    private static final boolean NEW_UI = true;
+
     public static void main(String[] args) {
-        ApplicationLauncher.launchApp(args);
+        if (NEW_UI) {
+            GraphEditorDemo.launchApp(args);
+        } else {
+            ApplicationLauncher.launchApp(args);
+        }
     }
 
 }
