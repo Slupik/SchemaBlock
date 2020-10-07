03c2bb5d00a664aee395d8195090f40e2d1b0edc
Slupik <Slupik98@gmail.com>
2020-10-07 23:08:23 +0200

    chore: Save program to single jar

--- UltimateDetail/src/main/kotlin/io/github/slupik/schemablock/Main.java
+++ UltimateDetail/src/main/kotlin/io/github/slupik/schemablock/Main.java
@@ -4,7 +4,7 @@ import de.tesis.dynaware.grapheditor.demo.GraphEditorDemo;
 
 public class Main {
 
-    private static final boolean NEW_UI = false;
+    private static final boolean NEW_UI = true;
 
     public static void main(String[] args) {
         if (NEW_UI) {
