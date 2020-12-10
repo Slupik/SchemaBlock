009354c5960248fd80da7fca6710005821279dbd
Slupik <Slupik98@gmail.com>
2020-12-10 21:00:44 +0100

    feat: improve value converter

--- WitLang-Model/src/main/kotlin/io/github/slupik/schemablock/newparser/memory/element/ValueConverter.java
+++ WitLang-Model/src/main/kotlin/io/github/slupik/schemablock/newparser/memory/element/ValueConverter.java
@@ -1,5 +1,7 @@
 package io.github.slupik.schemablock.newparser.memory.element;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * All rights reserved & copyright ©
  */
@@ -12,27 +14,35 @@ class ValueConverter {
         String parsed = ((String) value);
         switch (type) {
             case STRING: {
+                if (StringUtils.isBlank(parsed)) return "";
                 return value;
             }
             case DOUBLE: {
+                if (StringUtils.isBlank(parsed)) return 0D;
                 return Double.parseDouble(parsed);
             }
             case FLOAT: {
+                if (StringUtils.isBlank(parsed)) return 0F;
                 return ((float) Double.parseDouble(parsed));
             }
             case LONG: {
+                if (StringUtils.isBlank(parsed)) return 0L;
                 return ((long) Double.parseDouble(parsed));
             }
             case INTEGER: {
+                if (StringUtils.isBlank(parsed)) return 0;
                 return ((int) Double.parseDouble(parsed));
             }
             case SHORT: {
+                if (StringUtils.isBlank(parsed)) return (short) 0;
                 return ((short) Double.parseDouble(parsed));
             }
             case BYTE: {
+                if (StringUtils.isBlank(parsed)) return (byte) 0;
                 return ((byte) Double.parseDouble(parsed));
             }
             case BOOLEAN: {
+                if (StringUtils.isBlank(parsed)) return false;
                 return Boolean.parseBoolean(parsed);
             }
         }
