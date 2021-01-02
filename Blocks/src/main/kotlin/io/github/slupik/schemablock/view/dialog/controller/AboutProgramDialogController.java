9c96a3985d1a0641cbc4eabf023e47bbb5a41758
Slupik <Slupik98@gmail.com>
2021-01-03 00:48:28 +0100

    feat: base layout for info about program

---
+++ Blocks/src/main/kotlin/io/github/slupik/schemablock/view/dialog/controller/AboutProgramDialogController.java
@@ -0,0 +1,23 @@
+package io.github.slupik.schemablock.view.dialog.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * All rights reserved & copyright ©
+ */
+public class AboutProgramDialogController implements Initializable {
+
+    @FXML
+    private WebView webView;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
