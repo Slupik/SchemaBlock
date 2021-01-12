09c505c79abae7f05519ebb3d8764ab1c51184e6
Slupik <Slupik98@gmail.com>
2021-01-12 21:02:23 +0100

    feat: attach eclipse license

--- Blocks/src/main/kotlin/io/github/slupik/schemablock/view/dialog/controller/AboutProgramDialogController.java
+++ Blocks/src/main/kotlin/io/github/slupik/schemablock/view/dialog/controller/AboutProgramDialogController.java
@@ -17,30 +17,36 @@ public class AboutProgramDialogController implements Initializable {
 
     private static final String PATH = "/de/tesis/dynaware/grapheditor/demo/dialog/about/";
     private static final String USED_LIBRARIES_FILENAME = "libraries.html";
+    private static final String ECLIPSE_LICENSE_FILENAME = "eclipse_license.html";
 
     @FXML
     private WebView wvUsedLibraries;
 
+    @FXML
+    private WebView wvEclipseLicense;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        wvUsedLibraries.getEngine().loadContent(getUsedLibraries());
-        enableOpeningLinksInSystemBrowser(wvUsedLibraries);
+        setupWebView(wvUsedLibraries, USED_LIBRARIES_FILENAME);
+        setupWebView(wvEclipseLicense, ECLIPSE_LICENSE_FILENAME);
+    }
+
+    private void setupWebView(WebView webView, String filename) {
+        String content = loadInternalFileContent(PATH + filename);
+        webView.getEngine().loadContent(content);
+        enableOpeningLinksInSystemBrowser(webView);
     }
 
     private void enableOpeningLinksInSystemBrowser(WebView webView) {
         webView.getEngine().getLoadWorker().stateProperty().addListener(new HyperLinkRedirectListener(webView));
     }
 
-    private String getUsedLibraries() {
+    private String loadInternalFileContent(String path) {
         try {
-            return loadInternalFileContent(PATH + USED_LIBRARIES_FILENAME);
+            return IOUtils.toString(getClass().getResourceAsStream(path), StandardCharsets.UTF_8.name());
         } catch (IOException e) {
             e.printStackTrace();
             return "";
         }
-    }
-
-    private String loadInternalFileContent(String path) throws IOException {
-        return IOUtils.toString(getClass().getResourceAsStream(path), StandardCharsets.UTF_8.name());
     }
 }
