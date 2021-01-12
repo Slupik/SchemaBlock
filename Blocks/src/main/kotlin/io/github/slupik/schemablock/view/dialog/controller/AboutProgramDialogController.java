3be9a2d2288662fc88026325ad0a3a1a3c428711
Slupik <Slupik98@gmail.com>
2021-01-12 22:20:00 +0100

    feat: add missed copyright data and missed licenses

--- Blocks/src/main/kotlin/io/github/slupik/schemablock/view/dialog/controller/AboutProgramDialogController.java
+++ Blocks/src/main/kotlin/io/github/slupik/schemablock/view/dialog/controller/AboutProgramDialogController.java
@@ -17,21 +17,37 @@ public class AboutProgramDialogController implements Initializable {
 
     private static final String PATH = "/de/tesis/dynaware/grapheditor/demo/dialog/about/";
     private static final String USED_LIBRARIES_FILENAME = "libraries.html";
-    private static final String ECLIPSE_LICENSE_FILENAME = "eclipse_license.html";
+    private static final String ECLIPSE1_LICENSE_FILENAME = "eclipse1_license.html";
+    private static final String ECLIPSE2_LICENSE_FILENAME = "eclipse2_license.html";
     private static final String APACHE_LICENSE_FILENAME = "apache_license.html";
+    private static final String OPENFX_LICENSE_FILENAME = "openfx_license.html";
+    private static final String MIT_LICENSE_FILENAME = "mit_license.html";
+    private static final String BSD3_LICENSE_FILENAME = "bsd3_license.html";
 
     @FXML
     private WebView wvUsedLibraries;
     @FXML
-    private WebView wvEclipseLicense;
+    private WebView wvEclipse1License;
+    @FXML
+    private WebView wvEclipse2License;
     @FXML
     private WebView wvApacheLicense;
+    @FXML
+    private WebView wvOpenFxLicense;
+    @FXML
+    private WebView wvMitLicense;
+    @FXML
+    private WebView wvBsd3License;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         setupWebView(wvUsedLibraries, USED_LIBRARIES_FILENAME);
-        setupWebView(wvEclipseLicense, ECLIPSE_LICENSE_FILENAME);
+        setupWebView(wvEclipse1License, ECLIPSE1_LICENSE_FILENAME);
+        setupWebView(wvEclipse2License, ECLIPSE2_LICENSE_FILENAME);
         setupWebView(wvApacheLicense, APACHE_LICENSE_FILENAME);
+        setupWebView(wvOpenFxLicense, OPENFX_LICENSE_FILENAME);
+        setupWebView(wvMitLicense, MIT_LICENSE_FILENAME);
+        setupWebView(wvBsd3License, BSD3_LICENSE_FILENAME);
     }
 
     private void setupWebView(WebView webView, String filename) {
