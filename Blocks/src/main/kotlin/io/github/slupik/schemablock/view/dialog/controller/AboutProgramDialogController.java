25e91eb861859d67bfa0873f6632e71b6f926f19
Slupik <Slupik98@gmail.com>
2021-01-12 20:54:18 +0100

    feat: display list of used libraries

--- Blocks/src/main/kotlin/io/github/slupik/schemablock/view/dialog/controller/AboutProgramDialogController.java
+++ Blocks/src/main/kotlin/io/github/slupik/schemablock/view/dialog/controller/AboutProgramDialogController.java
@@ -3,8 +3,11 @@ package io.github.slupik.schemablock.view.dialog.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.web.WebView;
+import org.apache.commons.io.IOUtils;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ResourceBundle;
 
 /**
@@ -12,12 +15,27 @@ import java.util.ResourceBundle;
  */
 public class AboutProgramDialogController implements Initializable {
 
-    @FXML
-    private WebView webView;
+    private static final String PATH = "/de/tesis/dynaware/grapheditor/demo/dialog/about/";
+    private static final String USED_LIBRARIES_FILENAME = "libraries.html";
 
+    @FXML
+    private WebView wvUsedLibraries;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        wvUsedLibraries.getEngine().loadContent(getUsedLibraries());
+    }
 
+    private String getUsedLibraries() {
+        try {
+            return loadInternalFileContent(PATH + USED_LIBRARIES_FILENAME);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    private String loadInternalFileContent(String path) throws IOException {
+        return IOUtils.toString(getClass().getResourceAsStream(path), StandardCharsets.UTF_8.name());
     }
 }
